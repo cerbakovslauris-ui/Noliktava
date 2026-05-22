e40b9b4590fcdb483d6796420aad22c5ccc46477
cerbakovslauris-ui <cerbakovslauris@gmail.com>
2026-05-22 08:42:22 +0300

    ds

--- Skats/admin.php
+++ Skats/admin.php
@@ -12,6 +12,7 @@ $lietotajaVardsRedzams = $lietotajaVards;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link rel="stylesheet" href="../Css/skats.css">
+    <link rel="stylesheet" href="../Css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
@@ -24,5 +25,25 @@ $lietotajaVardsRedzams = $lietotajaVards;
             <h1>Admin</h1>
         </div>
     </header>
+
+    <main class="admin-lapa">
+        <aside class="admin-nav-bar" aria-label="Admina navigācija">
+            <h2>Admina panelis</h2>
+            <nav>
+                <ul>
+                    <li><a class="active" href="admin.php"><i class="fa fa-dashboard" aria-hidden="true"></i>Panelis</a></li>
+                    <li><a href="#lietotaji"><i class="fa fa-users" aria-hidden="true"></i>Lietotāju pārvaldība</a></li>
+                    <li><a href="#lomas"><i class="fa fa-id-badge" aria-hidden="true"></i>Lomu un piekļuves tiesības</a></li>
+                    <li><a href="#preces"><i class="fa fa-cubes" aria-hidden="true"></i>Preču pārvaldība</a></li>
+                    <li><a href="#plaukti"><i class="fa fa-th-large" aria-hidden="true"></i>Plauktu kartēšana</a></li>
+                    <li><a href="#pasutijumi"><i class="fa fa-clipboard" aria-hidden="true"></i>Pasūtījumu kontrole</a></li>
+                    <li><a href="#atskaites"><i class="fa fa-line-chart" aria-hidden="true"></i>Atskaites un statistika</a></li>
+                    <li><a href="#zurnals"><i class="fa fa-history" aria-hidden="true"></i>Darbību žurnāls</a></li>
+                </ul>
+            </nav>
+        </aside>
+
+        
+    </main>
 </body>
 </html>
